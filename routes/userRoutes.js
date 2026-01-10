b5e03faea7bffb21edf9ca70b78d61cc9785ff2f
tohbay <tobechukwuobitube@gmail.com>
2026-01-10 15:56:31 +0100

    setup the following endpoints - login - logout - follow/unfollow - update profile - get profile with helpers and middlewares

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { signupUser } from "../controllers/userController.js";
+import {
+  signupUser,
+  loginUser,
+  logoutUser,
+  followUnfollowUser,
+  updateUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/profile/:username", getUserProfile);
 router.post("/signup", signupUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/update/:id", protectRoute, updateUser);
 
 export default router;
